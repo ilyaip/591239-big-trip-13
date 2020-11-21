a35cdfbe16f1aadf811a8433a18177bc7d0e19fa
ilya <65188527+ilyaip@users.noreply.github.com>
2020-11-21 23:02:36 +0300

    module1-task2-correct

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import {createTripInfoTemplate} from "./view/trip-info.js";
 import {createTripMenuTemplate} from "./view/menu.js";
 import {createTripFiltersTemplate} from "./view/filters.js";
 import {createTripSortTemplate} from "./view/sort.js";
-import {createTripContentTemplate} from "./view/content.js";
+import {createTripAddEventTemplate} from "./view/add.js";
+import {createTripEditEventTemplate} from "./view/edit.js";
+import {createTripEventTemplate} from "./view/event.js";
+
+const EVENT_COUNT = 3;
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -23,4 +27,13 @@ const siteMainElement = document.querySelector("main");
 const siteSortElement = siteMainElement.querySelector(".trip-events");
 
 render(siteSortElement, createTripSortTemplate(), "beforeend");
-render(siteSortElement, createTripContentTemplate(), "beforeend");
+render(siteSortElement, createTripAddEventTemplate(), "beforeend");
+
+const siteEventListElement = siteMainElement.querySelector(".trip-events__list");
+
+render(siteEventListElement, createTripEditEventTemplate(), "beforeend");
+
+for (let i = 0; i < EVENT_COUNT; i++) {
+  render(siteEventListElement, createTripEventTemplate(), "beforeend");
+}
+
